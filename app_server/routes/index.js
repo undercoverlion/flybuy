27e1a040d60db25dd3f69400417cddef43c3718c
goutham sai <128449390+undercoverlion@users.noreply.github.com>
2023-08-23 12:12:14 +0530

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,19 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var ctrllocation = require('../controllers/location');
-var ctrlothers = require('../controllers/others');
 
-/* GET Locations page */
-router.get('/location', ctrllocation.locationInfo);
-router.get('/location2', ctrllocation.locationInfo2);
-router.get('/location3', ctrllocation.locationInfo3);
-router.get('/location/review/new', ctrllocation.addReview);
-
-/*GET others page */
-router.get('/about', ctrlothers.about);
-
-router.get('/signin', ctrlothers.signin);
-
-router.get('/register', ctrlothers.register);
 
 module.exports = router;
